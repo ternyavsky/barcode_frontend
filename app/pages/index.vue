67f49e118cb59490995e18d1a97e04a0b6e7150d
ternyavsky <ilya.ternyavsky@yandex.ru>
2025-08-11 15:32:34 +0300

    header

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,23 +1,11 @@
 <template>
-  <div class="flex h-screen bg-black">
-    <div class="flex-1 bg-black text-white p-6">
-      <div class="text-center">
-        <h1 class="text-3xl font-bold mb-4">Welcome to Barcode AI</h1>
-        <p class="text-gray-400 mb-8">Your smart barcode and MRZ solution</p>
-        <UButton
-          color="success"
-          variant="solid"
-          size="lg"
-          @click="navigateTo('/bulk-generation')"
-        >
-          Get Started
-        </UButton>
-      </div>
-    </div>
+  <div class="flex flex-col gap-4">
+    <Header />
   </div>
 </template>
 
 <script setup lang="ts">
+import Header from "~/widgets/header.vue";
 useHead({
   title: "Barcode AI - Smart Barcode Solution",
   meta: [
---
+++ app/widgets/header.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="w-full px-9 py-[24px] flex justify-between items-center relative">
+    <h1 class="text-2xl font-roboto uppercase font-bold">Logo</h1>
+    <div
+      class="gap-[30px] fixed right-0 w-full justify-center hidden desktop:flex"
+    >
+      <NuxtLink :to="localePath('/')"> {{ t("home.home") }} </NuxtLink>
+      <NuxtLink :to="localePath('/barcodes')"> {{ t("home.store") }} </NuxtLink>
+      <NuxtLink :to="localePath('/mrz')"> {{ t("home.discussion") }} </NuxtLink>
+      <NuxtLink :to="localePath('/mrz')"> {{ t("home.faq") }} </NuxtLink>
+    </div>
+    <div class="flex gap-[10px] desktop:gap-[20px] items-center">
+      <div class="items-center hidden desktop:flex">
+        <span class="text-sm">EN</span>
+        <UIcon name="i-heroicons-chevron-down" class="h-4 w-4" />
+      </div>
+      <img
+        src="/assets/svg/dark/Button.svg"
+        alt="Search"
+        class="cursor-pointer my-auto w-9 pt-[6px]"
+      />
+      <div
+        class="border-bg-tertiary border-[1px] rounded-[16px] px-[14px] py-3 font-semibold text-sm"
+      >
+        <h4>1700 <span class="text-text-tertiary">$</span></h4>
+      </div>
+      <div class="relative">
+        <img
+          src="/assets/svg/dark/Subtract.svg"
+          alt="User"
+          class="w-6 h-6 cursor-pointer"
+        />
+        <div
+          class="absolute top-[-5px] right-[-5px] w-4 h-4 bg-bg-secondary rounded-[12px] flex items-center justify-center p-2"
+        >
+          <p class="text-[8px] font-semibold">13</p>
+        </div>
+      </div>
+      <img
+        src="/assets/svg/dark/account.svg"
+        alt="Search"
+        class="cursor-pointer my-auto w-8 mt-[6px] hidden desktop:block"
+      />
+      <img
+        src="/assets/svg/dark/menu-line-horizontal.svg"
+        alt="Search"
+        class="cursor-pointer my-auto w-9 mt-[6px] block desktop:hidden"
+      />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+const localePath = useLocalePath();
+const { t } = useI18n();
+</script>
